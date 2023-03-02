e39edea9349502a40b1b570a6c15c8dbb87a6fea
Josep Maria Martinez <extern.jose-maria.martinez@europcar.com>
2023-03-02 12:06:50 +0100

    Changed vpn-status to new ip (ditched ifconfig)

--- config/polybar/scripts/vpn-status.sh
+++ config/polybar/scripts/vpn-status.sh
@@ -1,9 +1,10 @@
 #!/bin/sh
 
-IFACE=$(ifconfig | grep ppp | awk '{print $1}')
+# IFACE=$(ip addr show | grep ppp | awk '{print $2}')
+IFACE=$(ip addr show | grep ppp0 | awk '{print $2}' | head -n 1 | cut -f1 -d ':')
 
 if [ "$IFACE" = "ppp0" ]; then
-    echo "%{u#55aa55}$(ifconfig ppp0 | grep inet | awk '{print $2}' | cut -f2 -d ':')%{u-}"
+    echo "%{u#55aa55}$(ip addr show ppp0 | grep inet | awk '{print $2}' | cut -f2 -d ':')%{u-}"
 else
     echo "%{F#FF0000}%{u#FF0000}VPN DISCONNECTED%{u-}%{F-}"
 fi
